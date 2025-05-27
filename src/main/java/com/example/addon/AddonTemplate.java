3573fd9d9fc4908089d41ff115b5a0148b81ad83
Knoxius <tuxltuxl001@gmail.com>
2025-05-27 15:14:49 +0530

    Update AddonTemplate.java

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -4,12 +4,13 @@ import com.example.addon.modules.FlexSpammer;
 import com.mojang.logging.LogUtils;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.systems.modules.Category;
+import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.slf4j.Logger;
 
 public class AddonTemplate extends MeteorAddon {
     public static final Logger LOG = LogUtils.getLogger();
-    public static final Category CATEGORY = new Category("Misc");
+    public static final Category CATEGORY = Categories.Misc;
 
     @Override
     public void onInitialize() {
